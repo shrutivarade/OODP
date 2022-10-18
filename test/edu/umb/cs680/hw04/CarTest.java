e36c9cb8528dd37adee442d33debb4a97a30dc82
shrutivarade <shrutivarade27@gmail.com>
2022-10-18 12:59:09 -0400

    Commit in OODP dir with HW04

---
+++ test/edu/umb/cs680/hw04/CarTest.java
@@ -0,0 +1,21 @@
+package edu.umb.cs680.hw04;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    private String[] carToStringArray(Car c){
+        String[] strarr = {c.getMake(), c.getModel(), String.valueOf(c.getYear())};
+       return(strarr);
+    }
+
+    @Test
+    public void verifyCarEqualityWithMakeModelYear(){
+        String[] expected = new String[]{"Toyota", "RAV4","2018"};
+        Car actual = new Car("Toyota", "RAV4",2018);
+        assertArrayEquals(expected, carToStringArray(actual));
+
+
+    }
+}
