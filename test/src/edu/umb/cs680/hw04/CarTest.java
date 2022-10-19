401b780013f386dfa70505c35a3140228d924478
shrutivarade <shrutivarade27@gmail.com>
2022-10-19 00:04:21 -0400

    Commit in OODP dir with HW04 assertArrayEqual

--- test/src/edu/umb/cs680/hw04/CarTest.java
+++ test/src/edu/umb/cs680/hw04/CarTest.java
@@ -1,13 +1,15 @@
-package edu.umb.cs680.hw04;
+package src.edu.umb.cs680.hw04;
 
+import edu.umb.cs680.hw04.Car;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class CarTest {
 
     private String[] carToStringArray(Car c){
         String[] strarr = {c.getMake(), c.getModel(), String.valueOf(c.getYear())};
-        return(strarr);
+       return(strarr);
     }
 
     @Test
@@ -18,4 +20,18 @@ class CarTest {
 
 
     }
+
+    /*
+
+    private String[] carToStringArray(Car c){
+        String[] strarr = {c.getMake(), c.getModel(),String.valueOf(c.getMileage()), String.valueOf(c.getYear()), String.valueOf(c.getPrice())};
+       return(strarr);
+    }
+
+    @Test
+    public void verifyCarEqualityWithMakeMileagePrice(){
+        String[] expected = new String[]{"BMW", "60","900000"};
+        Car actual = new Car("BMW", "M3",60,2023,900000);
+        assertArrayEquals(expected, carToStringArray(actual),"Model Matched");
+    }*/
 }
