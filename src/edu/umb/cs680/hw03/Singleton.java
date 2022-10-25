4e561777a8a716b52c146c8ed9c4d0150dadf272
shrutivarade <shrutivarade27@gmail.com>
2022-10-25 12:11:34 -0400

    Commit in OODP dir with HW03 Singleton

--- src/edu/umb/cs680/hw03/Singleton.java
+++ src/edu/umb/cs680/hw03/Singleton.java
@@ -1,16 +1,16 @@
 package edu.umb.cs680.hw03;
 
 public class Singleton {
-    private static Singleton s_inst = null;
+    private static Singleton inst = null;
 
     private Singleton() {
     }
 
     public static Singleton getInstance()
     {
-        if (s_inst == null)
-            s_inst = new Singleton();
-        return s_inst;
+        if (inst == null)
+            inst = new Singleton();
+        return inst;
 
     }
 
