5c0b4599195af118488a73f91e4ce15217a0441d
Shruti Varade <37963866+shrutivarade@users.noreply.github.com>
2022-11-06 21:22:22 -0500

    Delete src/edu/umb/cs680 directory

--- src/edu/umb/cs680/hw05/Authenticator.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.hw05;
-
-public class Authenticator {
-
-    public static boolean authenticate(SecurityContext ctx, EncryptedString pwd){
-        return true;
-    }
-
-}
--- src/edu/umb/cs680/hw06/State.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.hw06;
-
-public interface State {
-
-    public void login(EncryptedString pwd);
-    public void logout();
-
-
-}
